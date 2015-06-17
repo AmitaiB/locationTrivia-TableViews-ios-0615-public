66d0361d1b946843e0ab55affee2a9ccb02573b1
AmitaiB <blickstein@gmail.com>
2015-06-17 18:31:36 -0400

    halfway through setup

---
+++ locationTrivia-tableviews/FISLocationsTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  FISLocationsTableViewController.h
+//  locationTrivia-tableviews
+//
+//  Created by Amitai Blickstein on 6/17/15.
+//  Copyright (c) 2015 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FISLocation.h"
+#import "FISTrivia.h"
+
+@interface FISLocationsTableViewController : UITableViewController
+
+@end
